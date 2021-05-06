d03884114c26219a4d9cce4e2f930a659b8527a7
Osman İsmayılov <osmanismayilov@Osmans-MacBook-Pro.local>
2021-05-06 22:05:30 +0400

    Deployment Network

--- example-memorystore/redis/variables.tf
+++ example-memorystore/redis/variables.tf
@@ -1,6 +1,6 @@
 variable "project" {}
 variable "region" {}
 variable "target_service_account" {}
-variable "name" {}
+variable "name_redis" {}
 variable "memory_size_gb" {}
 variable "enable_apis" {}
--- providers.tf
+++ providers.tf
@@ -5,8 +5,7 @@ terraform {
       version = "3.65.0"
     }
   }
-  backend "gcs" {
-    bucket  = "tf-gcs--backend"
+  backend "local" {
   }
 }
 
@@ -22,4 +21,14 @@ provider "google" {
   project = var.project_id
   zone    = var.region
   access_token = data.google_service_account_access_token.default.access_token
+}
+provider "kubernetes" {
+  load_config_file       = false
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+provider "null" {
+  version = "~> 2.1"
 }
